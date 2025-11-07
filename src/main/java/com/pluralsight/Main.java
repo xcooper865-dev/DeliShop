c85b05694dd3361813c69d03b7297c43470c174b
xaviercooper <xcooper@my.yearupunited.org>
2025-11-07 08:43:39 -0500

    Just getting started

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+public class Main {
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("""
+                    --------------------------
+                          Home Screen
+                    ---------------------------
+                    1) New order
+                    0) Exit      
+                    """);
+        }
+    }
+}
